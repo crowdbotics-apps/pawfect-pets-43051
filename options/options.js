6537ce3018d0a82f1097b42e0e60231356a47493
Crowdbotics <team@crowdbotics.com>
2023-08-24 12:01:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pawfect-pets-43051",
+  url: "https://pawfect-pets-43051.botics.co",
+  api: "https://pawfect-pets-43051.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
